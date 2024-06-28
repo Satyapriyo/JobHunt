22592c6afc14b31c3d4956b27f0d3ec5845c74d3
Satyapriyo Biswas <satyapriyoak47@gmail.com>
2024-06-28 21:31:12 +0530

    added the hooks

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './popularjobs.style'
-
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import { useRouter } from "expo-router";
+import styles from "./popularjobs.style";
+import { COLORS, SIZES } from "../../../constants";
+import PolularJobCards from "../../common/cards/popular/PopularJobCard";
+import { ColorSpace } from "react-native-reanimated";
 const Popularjobs = () => {
+  const error = false;
+  const router = useRouter();
+  const isLoading = false;
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popularjobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something wrong ...</Text>
+        ) : (
+          <FlatList
+            data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+            renderItem={({ item }) => <PolularJobCards item={item} />}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Popularjobs
+export default Popularjobs;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -10,8 +10,19 @@ import {
 import { useRouter } from "expo-router";
 import styles from "./welcome.style";
 import { icons, SIZES } from "../../../constants";
+import { sin } from "react-native-reanimated";
+const jobsType = [
+  "Full-time",
+  "Part-time",
+  "Internship",
+  "Temporary",
+  "Contract",
+  "Commission",
+  "Volunteer",
+];
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time");
   return (
     <View>
       <View style={styles.container}>
@@ -20,9 +31,43 @@ const Welcome = () => {
       </View>
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput} />
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="what are you looking for ?"
+          />
         </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          ></Image>
+        </TouchableOpacity>
       </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobsType}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push("/search/${item}");
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+
+          keyExtractor={item=>item}
+          contentContainerStyle={{columnGap :SIZES.small}}
+          horizontal
+        />
+      </View>  
     </View>
   );
 };
